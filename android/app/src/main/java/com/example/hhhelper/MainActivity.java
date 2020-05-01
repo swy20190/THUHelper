7fc7eb06e2563d74a944ce112a4f732aa08a132e
swy20190 <K201410109@163.com>
2020-05-01 20:39:24 +0800

    tab and viewPager in the main page

---
+++ android/app/src/main/java/com/example/hhhelper/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.hhhelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private FragmentPagerAdapter mPageAdapter;
+    private ArrayList<String> titleList = new ArrayList<>();
+    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+        titleList.clear();
+        titleList.add("我的下单");
+        titleList.add("订单市场");
+        titleList.add("我的接单");
+
+        fragmentList.clear();
+        fragmentList.add(new Fragment_send());
+        fragmentList.add(new Fragment_market());
+        fragmentList.add(new Fragment_receive());
+
+        mPageAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return fragmentList.get(position);
+            }
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position){
+                return titleList.get(position);
+            }
+            @Override
+            public long getItemId(int position){
+                return position;
+            }
+
+            @Override
+            public int getCount() {
+                return titleList.size();
+            }
+        };
+        viewPager.setAdapter(mPageAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+}
