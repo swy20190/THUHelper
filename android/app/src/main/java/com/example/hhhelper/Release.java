9831efe8797ca83dbe89ef0848ef7007ab4a6eaf
swy20190 <K201410109@163.com>
2020-05-02 23:12:51 +0800

    release skeleton finished

--- android/app/src/main/java/com/example/hhhelper/Release.java
+++ android/app/src/main/java/com/example/hhhelper/Release.java
@@ -2,7 +2,11 @@ package com.example.hhhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Release extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class Release extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_release);
+        EditText editTitle = (EditText) findViewById(R.id.release_title);
+        EditText editDetail = (EditText)findViewById(R.id.release_detail);
+        EditText editDDL = (EditText) findViewById(R.id.release_ddl);
+        EditText editBonus = (EditText) findViewById(R.id.release_bonus);
+        Button releaseButton = (Button) findViewById(R.id.release_button);
+        releaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //提交
+                //如果成功
+                Intent intent = new Intent(Release.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
