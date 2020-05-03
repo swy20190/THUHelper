cac89f6ad9298ea533027780df7ba1457fb4cd7a
swy20190 <K201410109@163.com>
2020-05-03 17:44:36 +0800

    ddl time picker

--- android/app/src/main/java/com/example/hhhelper/Release.java
+++ android/app/src/main/java/com/example/hhhelper/Release.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,14 +12,19 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
+import java.util.Timer;
 
 public class Release extends AppCompatActivity {
     private Calendar mCalendar;
     private int mDate;
     private int mMonth;
     private int mYear;
+
+    private int mHour;
+    private int mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,8 @@ public class Release extends AppCompatActivity {
         mDate = mCalendar.get(Calendar.DATE);
         mMonth = mCalendar.get(Calendar.MONTH)+1;
         mYear = mCalendar.get(Calendar.YEAR);
+        mHour = mCalendar.get(Calendar.HOUR_OF_DAY);
+        mMinute = mCalendar.get(Calendar.MINUTE);
         setContentView(R.layout.activity_release);
         EditText editTitle = (EditText) findViewById(R.id.release_title);
         EditText editDetail = (EditText)findViewById(R.id.release_detail);
@@ -45,6 +53,22 @@ public class Release extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+
+        final TextView ddlTime = (TextView)findViewById(R.id.release_ddl_time);
+        ddlTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(Release.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                ddlTime.setText(hourOfDay+" : "+minute);
+                            }
+                        },mHour,mMinute,true);
+                timePickerDialog.show();
+            }
+        });
+
         Button releaseButton = (Button) findViewById(R.id.release_button);
         releaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
