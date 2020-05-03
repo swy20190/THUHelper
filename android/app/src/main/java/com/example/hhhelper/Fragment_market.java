e6ca28470535283b17949f8632c20049bc7ba82f
swy20190 <K201410109@163.com>
2020-05-03 13:48:33 +0800

    swipe refresh test

--- android/app/src/main/java/com/example/hhhelper/Fragment_market.java
+++ android/app/src/main/java/com/example/hhhelper/Fragment_market.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 
@@ -18,11 +19,20 @@ public class Fragment_market extends Fragment {
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerViewAdapter mAdapter;
     private View mainView;
+    private SwipeRefreshLayout swipeRefreshLayout;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState){
         mainView = inflater.inflate(R.layout.fragment_market,container,false);
         initView();
+        swipeRefreshLayout = (SwipeRefreshLayout) mainView.findViewById(R.id.fragment_market_swipe);
+        swipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.colorPrimary));
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshTickets();
+            }
+        });
         return mainView;
     }
     private void initView(){
@@ -30,10 +40,30 @@ public class Fragment_market extends Fragment {
         mLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL,false);
         ArrayList<Ticket> data = new ArrayList<>();
         for(int i=0;i<20;i++){
-            data.add(new Ticket("订单市场"+i,R.mipmap.ic_launcher));
+            data.add(new Ticket("订单市场"+(int)(Math.random()*20),R.mipmap.ic_launcher));
         }
         mAdapter = new RecyclerViewAdapter(data);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+    }
+    private void refreshTickets(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2000);  //进行网络查询
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initView();
+                        mAdapter.notifyDataSetChanged();
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
     }
 }
