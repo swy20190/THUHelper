dacf1c0f77b1bfa141752accee7a270a7870ce67
swy20190 <K201410109@163.com>
2020-05-03 09:17:50 +0800

    onclick to detail page

--- android/app/src/main/java/com/example/hhhelper/RecyclerViewAdapter.java
+++ android/app/src/main/java/com/example/hhhelper/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.hhhelper;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +19,12 @@ import java.util.List;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     private ArrayList<Ticket> mTicketList;
     public class ViewHolder extends RecyclerView.ViewHolder{
+        View ticketView;
         ImageView ticketImage;
         TextView ticketName;
         public ViewHolder(View view){
             super(view);
+            ticketView = view;
             ticketImage = (ImageView) view.findViewById(R.id.ticket_sender);
             ticketName = (TextView) view.findViewById(R.id.ticket_name);
         }
@@ -30,10 +33,21 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         mTicketList = ticketList;
     }
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType){
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.ticket_item,parent,false);
-        ViewHolder holder = new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.ticketView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Ticket ticket = mTicketList.get(position);
+                String uid = ticket.getUid();
+                Intent intent = new Intent(v.getContext(),DetailsActivity.class);
+                intent.putExtra("ticketUID",uid);
+                v.getContext().startActivity(intent);
+            }
+        });
         return holder;
     }
     @Override
--- android/app/src/main/java/com/example/hhhelper/Ticket.java
+++ android/app/src/main/java/com/example/hhhelper/Ticket.java
@@ -1,10 +1,12 @@
 package com.example.hhhelper;
 
 public class Ticket {
+    private String uid;
     private String name;
     private int imageId;
     public Ticket(String name, int imageId){
         this.name = name;
+        this.uid = name;//这里我暂时用name兼职一下uid
         this.imageId = imageId;
     }
     public String getName(){
@@ -12,5 +14,8 @@ public class Ticket {
     }
     public int getImageId(){
         return imageId;
+    }
+    public String getUid(){
+        return uid;
     }
 }
