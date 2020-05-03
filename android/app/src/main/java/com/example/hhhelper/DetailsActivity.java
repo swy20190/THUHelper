75786b6f96cdfc6bafdd0d8eed5dba0e7515bc59
swy20190 <K201410109@163.com>
2020-05-03 10:27:32 +0800

    detail page xml

--- android/app/src/main/java/com/example/hhhelper/DetailsActivity.java
+++ android/app/src/main/java/com/example/hhhelper/DetailsActivity.java
@@ -2,6 +2,7 @@ package com.example.hhhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        Intent intent = getIntent();
+        String uid = intent.getStringExtra("ticketUID");
+        // 使用这个UID进行订单的信息查询
+        // 应该需要开一个线程，然后更新ui
+
     }
 }
