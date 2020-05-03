3e119f4047e519efa01b2e0cebcd750afeffc606
swy20190 <K201410109@163.com>
2020-05-03 17:20:26 +0800

    ddl date picker

--- android/app/src/main/java/com/example/hhhelper/Release.java
+++ android/app/src/main/java/com/example/hhhelper/Release.java
@@ -2,22 +2,49 @@ package com.example.hhhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Release extends AppCompatActivity {
+    private Calendar mCalendar;
+    private int mDate;
+    private int mMonth;
+    private int mYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mCalendar = Calendar.getInstance();
+        mDate = mCalendar.get(Calendar.DATE);
+        mMonth = mCalendar.get(Calendar.MONTH)+1;
+        mYear = mCalendar.get(Calendar.YEAR);
         setContentView(R.layout.activity_release);
         EditText editTitle = (EditText) findViewById(R.id.release_title);
         EditText editDetail = (EditText)findViewById(R.id.release_detail);
-        EditText editDDL = (EditText) findViewById(R.id.release_ddl);
         EditText editBonus = (EditText) findViewById(R.id.release_bonus);
+        final TextView ddlDate = (TextView)findViewById(R.id.release_ddl_date);
+        ddlDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Release.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                ddlDate.setText(year+"."+month+"."+dayOfMonth);
+                            }
+                        },mYear,mMonth,mDate);
+                datePickerDialog.show();
+            }
+        });
         Button releaseButton = (Button) findViewById(R.id.release_button);
         releaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,4 +56,5 @@ public class Release extends AppCompatActivity {
             }
         });
     }
+
 }
