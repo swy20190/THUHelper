8fd22cabc4cee70f984def968c090a8d874535a0
swy20190 <K201410109@163.com>
2020-05-04 16:47:15 +0800

    chatting skeleton finish

--- android/app/src/main/java/com/example/hhhelper/ChattingActivity.java
+++ android/app/src/main/java/com/example/hhhelper/ChattingActivity.java
@@ -2,7 +2,9 @@ package com.example.hhhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ChattingActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ChattingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
+        Intent intent = getIntent();
+        String partnerName = intent.getStringExtra("partnerID");// 这里应该是通过id查找到昵称
+        TextView chatter = (TextView) findViewById(R.id.chatting_partner_name);
+        chatter.setText(partnerName);
     }
 }
