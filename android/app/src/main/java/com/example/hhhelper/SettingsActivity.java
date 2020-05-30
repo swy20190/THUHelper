69340b83508fb4c7695f69e6354d7b1ad47a109d
swy20190 <K201410109@163.com>
2020-05-30 20:43:57 +0800

    自动登录机制完成，说明见doc/loginManagement.md

--- android/app/src/main/java/com/example/hhhelper/SettingsActivity.java
+++ android/app/src/main/java/com/example/hhhelper/SettingsActivity.java
@@ -3,18 +3,24 @@ package com.example.hhhelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = preferences.edit();
         EditText editTextNick = (EditText) findViewById(R.id.edit_nickname);
         EditText editTextMail = (EditText) findViewById(R.id.edit_mail);
         EditText editTextDorm = (EditText) findViewById(R.id.edit_dorm);
@@ -32,6 +38,9 @@ public class SettingsActivity extends AppCompatActivity {
                 else{
                     //检查原密码等等
                     //如果无误
+                    //上传新密码
+                    editor.putBoolean("isEnsured", false); //将登录检查置为否
+                    editor.commit();
                     Intent intent = new Intent(SettingsActivity.this,LoginActivity.class);
                     startActivity(intent);
                 }
