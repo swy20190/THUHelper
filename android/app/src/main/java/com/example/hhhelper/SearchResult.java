37f8eae9d4bd18c6c0038a607465c3ba5d0cf0d8
swy20190 <K201410109@163.com>
2020-06-01 17:16:00 +0800

    搜索结果完成

--- android/app/src/main/java/com/example/hhhelper/SearchResult.java
+++ android/app/src/main/java/com/example/hhhelper/SearchResult.java
@@ -1,13 +1,20 @@
 package com.example.hhhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class SearchResult extends AppCompatActivity {
     private TextView title;
+    private RecyclerView mRecyclerView;
+    private ArrayList<Ticket> searchResults = new ArrayList<>();
+    private String queryString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +22,18 @@ public class SearchResult extends AppCompatActivity {
         setContentView(R.layout.activity_search_result);
         title = (TextView) findViewById(R.id.searchResult_title);
         Intent intent = getIntent();
-        String queryString = intent.getStringExtra("queryString");
+        queryString = intent.getStringExtra("queryString");
         title.setText(queryString);
-
+        initTickets();
+        mRecyclerView = (RecyclerView)findViewById(R.id.searchResult_recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(searchResults);
+        mRecyclerView.setAdapter(adapter);
+    }
+    private void initTickets(){
+        for(int i=0;i<20;i++){
+            searchResults.add(new Ticket(queryString+i,R.mipmap.ic_launcher));
+        }
     }
 }
